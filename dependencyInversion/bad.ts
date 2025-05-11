2038ca968b40f65135f33e3a7c43c3938f342017
marozex <49114932+marozex@users.noreply.github.com>
2025-05-11 15:32:01 +0900

    bad.ts例追加

---
+++ dependencyInversion/bad.ts
@@ -0,0 +1,37 @@
+class User {}
+
+class UserController {
+  private userController = new UserService()
+
+  create(user: User): User {
+    return this.userController.create(user)
+  }
+
+  findById(id: string): User {
+    return this.userController.findById(id)
+  }
+}
+
+class UserService {
+  private userRepository = new UserRdbRepository()
+
+  create(user: User): User {
+    return this.userRepository.create(user)
+  }
+
+  findById(id: string): User {
+    return this.userRepository.findById(id)
+  }
+}
+
+class UserRdbRepository {
+  create(user: User): User {
+    console.log("RDBでのユーザー作成処理")
+    return user
+  }
+
+  findById(id: string): User {
+    console.log("RDBでのユーザー検索処理")
+    return new User()
+  }
+}
