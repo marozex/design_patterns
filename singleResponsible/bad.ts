0da5633041c6557dd6aa87edaedbe196b7d357df
marozex <49114932+marozex@users.noreply.github.com>
2025-05-11 14:45:01 +0900

    badのセミコロン排除

--- singleResponsible/bad.ts
+++ singleResponsible/bad.ts
@@ -2,32 +2,31 @@ class Employee {
   constructor(private _name: string, private _department: string) {}
 
   get name() {
-    return this._name;
+    return this._name
   }
 
   private getRegularHours() {
-    console.log("労働時間を計算");
+    console.log("労働時間を計算")
   }
 
   calculatePay() {
-    this.getRegularHours();
-    console.log("時間から給与計算");
+    this.getRegularHours()
+    console.log("時間から給与計算")
   }
 
   reportHours() {
-    this.getRegularHours();
-    console.log("時間から労働時間報告");
+    this.getRegularHours()
+    console.log("時間から労働時間報告")
   }
 }
-
 const run = () => {
-  const emp1 = new Employee("田中", "営業");
+  const emp1 = new Employee("田中", "営業")
 
-  console.log("経理");
-  emp1.calculatePay();
+  console.log("経理")
+  emp1.calculatePay()
 
-  console.log("人事");
-  emp1.reportHours();
-};
+  console.log("人事")
+  emp1.reportHours()
+}
 
-run();
+run()
